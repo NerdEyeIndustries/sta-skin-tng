19cf27df31d989fe49a54119474e4f2bb64ec51b
NerdEyeIndustries <103020066+NerdEyeIndustries@users.noreply.github.com>
2022-07-09 20:07:34 -0700

    v0.1.1

---
+++ scripts/sta-skin-tng.js
@@ -0,0 +1,9 @@
+console.log("sta-skin-tng | TNG skin loaded.");
+
+Hooks.on("init", () => {
+  console.log("sta-skin-tng | Initializing TNG Skin...");
+});
+
+Hooks.on("ready", () => {
+  console.log("sta-skin-tng | Initialization complete!");
+});
